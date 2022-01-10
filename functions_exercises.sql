d5e185d797b0883b8e0757468ee0df6e23112e66
christopher <christopher.curran24@gmail.com>
2022-01-10 10:27:24 -0600

    Version 2

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -1,6 +1,21 @@
 # 2 
 SELECT CONCAT(first_name,SPACE(1), last_name) 'full_name' 
 FROM employees
-WHERE last_name LIKE ('E%') AND last_name LIKE ('%E')
+WHERE last_name LIKE ('E%') AND last_name LIKE ('%E');
 
+#3 
+SELECT UPPER(CONCAT(first_name,SPACE(1), last_name)) 'full_name' 
+FROM employees
+WHERE last_name LIKE ('E%') AND last_name LIKE ('%E');
+
+#4
+SELECT first_name, last_name, birth_date, hire_date, DATEDIFF(NOW(), hire_date)
+FROM employees
+WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31' 
+	AND (birth_date LIKE ('%12-25%'));
+
+#5
+
+SELECT emp_no, MIN(salary)
+FROM salaries;
 
