f613c546d1585685e4270d946f91f9bc736be36a
christopher <christopher.curran24@gmail.com>
2022-01-11 23:53:07 -0600

    Cont exercises

--- join_exercises.sql
+++ join_exercises.sql
@@ -25,7 +25,8 @@ JOIN dept_emp AS de
 JOIN departments AS d
   ON d.dept_no = de.dept_no
 JOIN dept_manager AS dm
-	ON dm.emp_no = e.emp_no;
+	ON dm.emp_no = de.emp_no
+WHERE dm.to_date like ('9999-%-%');
 #3 
 SELECT d.dept_name AS Department_Name, CONCAT(e.first_name, ' ', e.last_name) AS Department_Manager
 FROM employees AS e
@@ -35,18 +36,63 @@ JOIN departments AS d
   ON d.dept_no = de.dept_no
 JOIN dept_manager AS dm
 	ON dm.emp_no = e.emp_no
-WHERE gender = 'F';
-#4 
-SELECT t.title AS Title, COUNT(dp.dept_name) AS Count
-FROM employees AS e
+WHERE gender = 'F' AND dm.to_date LIKE ('9999-%-%')
+;
+#4 needs work
+SELECT t.title AS Title, COUNT(e.emp_no) AS COUNT
+FROM employees AS e, 
 JOIN titles AS t
 	ON t.emp_no = e.emp_no
-JOIN departments AS dp
-	ON dp.dept_no = 
-GROUP BY title;
+JOIN departments AS d
+	ON d.dept_no = de.dept_no
+JOIN dept_emp AS de
+  ON de.emp_no = e.emp_no
+GROUP BY title
+HAVING (dept_name = ('Customer Service') AND to_date = ('9999-01-01')); 
+#5
+SELECT d.dept_name AS Department_Name, CONCAT(e.first_name, ' ', e.last_name) AS Department_Manager, s.salary AS Salary
+FROM employees AS e
+JOIN dept_emp AS de
+  ON de.emp_no = e.emp_no
+JOIN departments AS d
+  ON d.dept_no = de.dept_no
+JOIN dept_manager AS dm
+	ON dm.emp_no = e.emp_no
+JOIN salaries AS s
+	ON s.emp_no = e.emp_no
+WHERE s.to_date like ('9999-%-%') AND dm.to_date LIKE ('9999-%-%');
+#6
+SELECT d.dept_no AS dept_no, d.dept_name AS depart_name, COUNT(*) AS num_employee
+FROM employees AS e
+JOIN dept_emp AS de
+  ON de.emp_no = e.emp_no
+JOIN departments AS d
+  ON d.dept_no = de.dept_no
+WHERE de.to_date LIKE ('9999-%-%')   
+GROUP BY dept_no, depart_name;
+#7  
+SELECT d.dept_name AS dept_name, AVG(s.salary) AS average_salary
+FROM employees AS e
+JOIN dept_emp AS de
+  ON de.emp_no = e.emp_no
+JOIN departments AS d
+  ON d.dept_no = de.dept_no
+JOIN salaries AS s
+	ON s.emp_no = e.emp_no
+WHERE s.to_date like ('9999-01-01') AND de.to_date LIKE ('9999-%-%')  
+GROUP BY dept_name;
+#8 
+SELECT e.first_name AS first_name, e.last_name AS last_name
+FROM employees AS e
+JOIN dept_emp AS de
+  ON de.emp_no = e.emp_no
+JOIN departments AS d
+  ON d.dept_no = de.dept_no
+JOIN salaries AS s
+	ON s.emp_no = e.emp_no
+WHERE s.to_date like ('9999-01-01') AND de.to_date LIKE ('9999-%-%') 
+GROUP BY first_name, last_name; 
 
-
-;
 
 
 
