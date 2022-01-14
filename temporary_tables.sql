325eb798f1aab26012a0e74547d0779d9f1fe58d
christopher <christopher.curran24@gmail.com>
2022-01-14 13:13:50 -0600

    Version #2

--- temporary_tables.sql
+++ temporary_tables.sql
@@ -1,11 +1,47 @@
 
+# 1
 USE employees;
 
-CREATE TEMPORARY TABLE my_database_with_permissions.employees_with_salaries AS 
+CREATE TEMPORARY TABLE innis_1667.employees_with_salaries AS 
 SELECT * FROM employees JOIN salaries USING(emp_no);
 
-USE my_database_with_permissions;
+USE innis_1667;
 SELECT * FROM employees_with_salaries;
 
-CREATE TEMPORARY TABLE employees_with_departments AS
+ALTER TABLE employees_with_salaries ADD dept_name VARCHAR (20);
+
+CREATE TEMPORARY TABLE employees_with_departments AS 
 SELECT first_name, last_name, dept_name
+FROM employees_with_salaries;
+
+SELECT * FROM employees_with_departments;
+
+ALTER TABLE employees_with_departments ADD full_name VARCHAR (50);
+
+UPDATE employees_with_departments
+SET full_name = CONCAT(first_name,'  ', Last_name);
+
+SELECT * FROM employees_with_departments;
+
+ALTER TABLE employees_with_departments DROP COLUMN first_name;
+ALTER TABLE employees_with_departments DROP COLUMN last_name;
+
+SELECT * FROM employees_with_departments;
+#
+
+#2
+USE sakila;
+SELECT DATABASE();
+
+CREATE TEMPORARY TABLE innis_1667.payment AS 
+SELECT * FROM payment;
+
+USE innis_1667;
+
+SELECT * FROM innis_1667.payment;
+
+
+
+
+
+
